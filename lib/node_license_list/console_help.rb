c463e714afddf0bf9fbb218b94bd4905ac8b2b3f
Albin <j.albin.willman@gmail.com>
2017-02-14 13:21:54 +0100

    Implements export option

--- lib/node_license_list/console_help.rb
+++ lib/node_license_list/console_help.rb
@@ -2,7 +2,14 @@ module NodeLicenseList
   class ConsoleHelp
 
     def self.run(opts = {})
-      puts '## license_list ##'
+      puts 'usage: license_list [--verbose][--help][--export output=file]'
+      puts ''
+      puts ''
+      puts 'Console output'
+      puts '  Red licenses indicate copyleft'.red
+      puts '  Yellow licenses are unkown to this gem'.yellow
+      puts '  Blue licenses indicate that the gem parsed the license file and guessed it is free to use'.blue
+      puts '  Green licenses indicate free to use'.green
     end
   end
 end
--- lib/node_license_list/file_writer.rb
+++ lib/node_license_list/file_writer.rb
@@ -1,8 +1,16 @@
+require 'csv'
 module NodeLicenseList
-
   class FileWriter
     def self.run(package_groups, path)
-      # TODO: implement export
+      CSV.open(path, "w", { col_sep: ';' }) do |csv|
+        csv << ['Package', 'License', 'Type']
+        package_groups.each do |license, packages|
+          level = LicenseClassifier.safety_level(license)
+          packages.each do |p|
+            csv << [p, license, level.to_s]
+          end
+        end
+      end
     end
   end
 end
